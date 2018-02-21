2f3518de191a9a1dc2aa95a9aeffa8a8a02e1615
Helen Varley <hvarley@gmail.com>
2018-02-21 10:31:30 +0000

    google analytics code

--- header.php
+++ header.php
@@ -18,6 +18,16 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114507650-1"></script>
+	<script>
+  	window.dataLayer = window.dataLayer || [];
+  	function gtag(){dataLayer.push(arguments);}
+  	gtag('js', new Date());
+
+  	gtag('config', 'UA-114507650-1');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
