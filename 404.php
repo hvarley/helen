91c049bcd6d07e0b62b800e2a881b7d412b13c8a
Helen Varley <hvarley@gmail.com>
2018-02-21 10:43:11 +0000

    updated 404 page

--- 404.php
+++ 404.php
@@ -1,60 +1,40 @@
 <?php
 /**
- * The template for displaying 404 pages (not found)
+ * The template for displaying all pages
  *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package helen
  */
 
 get_header(); ?>
 
+
+
 	<div id="primary" class="content-area">
+
+		<div class="title-bar" style="background-image: url('<?php bloginfo('template_directory'); ?>/dist/images/about-me-header.jpg')">
+			<div class="container">
+				<h1 class="page-title"></h1>
+			</div>
+		</div>
+
+		<div class="container main_content">
 		<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'helen' ); ?></h1>
-				</header><!-- .page-header -->
+			<h1>Oops! That page can't be found.</h1>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'helen' ); ?></p>
+			</main><!-- #main -->
 
-					<?php
-						get_search_form();
 
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+		</div>
+		</div><!-- #primary -->
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'helen' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
+	<?php
 
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'helen' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_footer();
+	get_footer();
